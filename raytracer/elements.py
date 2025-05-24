e113cb85e21f483a49e1ec5da1872fc510fea6d7
KingL0ui5 <liulouis1@gmail.com>
2025-05-24 18:39:50 +0100

    tasks 5-7

--- raytracer/elements.py
+++ raytracer/elements.py
@@ -4,6 +4,7 @@ L Liu 22/05
 """
 import numpy as np
 from raytracer.rays import Ray
+from raytracer.physics import refract 
 
 class OpticalElement:
     def intercept(self, ray):
@@ -62,6 +63,41 @@ class SphericalRefraction(OpticalElement):
         
         if intercept[0]**2 + intercept[1]**2 > (self.__aperture)**2:
             return None
-        
         return intercept
+    
+    def propagate_ray(self, ray): # test 
+        intercept = self.intercept(ray)
+        if intercept is None:
+            return None
         
+        if self.__curvature == 0:
+            normal = np.array([0., 0., 1.])
+        else:
+            R = 1. / self.__curvature
+            normal = intercept - np.array([0., 0., self.__z_0 + R])
+            normal /= np.linalg.norm(normal)
+        
+        refracted_direc = refract(ray.direc(), normal, self.__n_1, self.__n_2)
+        if refracted_direc is None:
+            return None
+        
+        ray.append(intercept, refracted_direc)     
+        
+
+class OutputPlane(OpticalElement):
+    def __init__(self, z_0: float):
+        self.__z_0 = z_0
+        
+    def z_0(self):
+        return self.__z_0
+    
+    def intercept(self, ray):
+        if ray.direc()[2] == 0: 
+            return None
+        k = (self.__z_0 - ray.pos()[2]) / ray.direc()[2]
+        intercept = ray.pos() + k * ray.direc()
+        return intercept
+    
+    def propagate_ray(self, ray):
+        intercept = self.intercept(ray)
+        ray.append(intercept, ray.direc()) # unchanged direction
--- raytracer/physics.py
+++ raytracer/physics.py
@@ -5,27 +5,24 @@ L Liu 23/05
 import numpy as np
 from raytracer.helpers import Utils as hlp 
         
-def refract(direc, normal, n_1, n_2):
+def refract(direc, normal, n_1, n_2): # test
     hlp.validate_vector(direc, "Direc")
     hlp.validate_vector(normal, "Normal")
     direc = hlp.normalise_vector(direc)
     normal = hlp.normalise_vector(normal)
-    
-    theta_1 = np.arccos(np.dot(direc, normal))
-    sin_theta_2 = (n_1 / n_2) * np.sin(theta_1)
-    if sin_theta_2 > 1:
+
+    eta = n_1 / n_2
+    cos_theta_1 = -np.dot(direc, normal)
+    sin2_theta_2 = eta**2 * (1 - cos_theta_1**2)
+
+    if sin2_theta_2 > 1.:
         return None
-    
-    theta_2 = np.arcsin(sin_theta_2)
-    ref_direc = (np.cos(theta_2 - theta_1) * normal) + (np.cos(theta_2) * direc)
-    
-    ref_direc /= np.linalg.norm(ref_direc)
-    
-    return (ref_direc)
 
-direc = np.array([0., 0., 1.])
-norm_lower = np.array([0., -1., -1.])
-norm_lower /= np.linalg.norm(norm_lower)
+    cos_theta_2 = np.sqrt(1 - sin2_theta_2)
+    ref_direc = eta * direc + (eta * cos_theta_1 - cos_theta_2) * normal 
+    
+    # ref_direc = (np.cos(theta_2 - theta_1) * normal) + (np.cos(theta_2) * direc)
 
-output = refract(direc=direc, normal=norm_lower, n_1=1.0, n_2=1.5) #, np.array([0., 0.29027623, 0.9569429]))
-print(output) 
+    hlp.normalise_vector(ref_direc)
+    
+    return ref_direc
